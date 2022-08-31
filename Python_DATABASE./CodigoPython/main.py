236752cd86f93b3c4ef1f9fd5b2336f05eb34e73
Daniel Borba <108240892+DanielBorbafs@users.noreply.github.com>
2022-08-31 09:55:07 -0300

    Update main.py

--- Python_DATABASE./CodigoPython/main.py
+++ Python_DATABASE./CodigoPython/main.py
@@ -1,1 +1,20 @@
 
+import mysql.connector
+from mysql.connector import errorcode
+import datetime
+
+mydb = mysql.connector.connect(
+    host="localhost",
+    user="root",
+    password="",
+    database="rh"
+)
+
+mycursor = mydb.cursor()
+
+mycursor.execute("Show tables;")
+
+myresult = mycursor.fetchall()
+
+for x in myresult:
+	print(x)
