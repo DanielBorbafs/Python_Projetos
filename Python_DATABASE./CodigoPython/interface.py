146024ab13c19808760c761e0d44a7faaad2e264
Daniel Borba <108240892+DanielBorbafs@users.noreply.github.com>
2022-08-31 09:54:33 -0300

    Create interface.py

---
+++ Python_DATABASE./CodigoPython/interface.py
@@ -0,0 +1,19 @@
+import tkinter
+from tkinter import *
+from tkinter import ttk
+
+cor1 = "#1e1f1e" # preta
+cor2 = "#feffff"
+cor3 = "#38576b"
+cor4 = "#ECEFF1"
+cor5 = "#FFAB40"
+
+
+interface = Tk()
+interface.title("RH - System")
+interface.geometry("800x500")
+interface.config(bg=cor1)
+
+
+
+interface.mainloop()
